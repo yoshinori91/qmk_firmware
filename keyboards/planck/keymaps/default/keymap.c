d7fd8bae9beac25fb5bdc7315e94c3ef65e6ea8f
p401670 <p401670@usernoMacBook-Pro.local>
2024-03-06 11:08:31 +0900

    wip

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
     KC_EQL,       KC_Q,  KC_W,  KC_E,          KC_R,               KC_T,  KC_Y,           KC_U,              KC_I,          KC_O,    KC_P,          KC_MINS,
-    LGUI(KC_SPC), KC_A,  KC_S,  KC_D,          KC_F,               KC_G,  KC_H,           KC_J,              KC_K,          KC_L,    OSM(MOD_LGUI), KC_ENT,
+    LCTL(KC_SPC), KC_A,  KC_S,  KC_D,          KC_F,               KC_G,  KC_H,           KC_J,              KC_K,          KC_L,    OSM(MOD_LGUI), KC_ENT,
     KC_GRV,       KC_Z,  KC_X,  KC_C,          KC_V,               KC_B,  KC_N,           KC_M,              KC_COMM,       KC_DOT,  KC_SLSH,       KC_BSLS,
     KC_NO,        KC_NO, KC_NO, OSM(MOD_LALT), LT(_LOWER, KC_ESC), KC_NO, LSFT_T(KC_SPC), LT(_RAISE,KC_TAB), OSM(MOD_LCTL), KC_LEFT, KC_NO,         KC_NO
 ),
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,   KC_NO, KC_AMPR, KC_ASTR, KC_NO,   KC_NO,   KC_NO,   KC_7, KC_8,  KC_9,  KC_NO,  KC_NO,
     KC_NO,   KC_NO, KC_DLR,  KC_PERC, KC_CIRC, KC_NO,   KC_NO,   KC_4, KC_5,  KC_6,  KC_HOME,KC_END,
     KC_NO,   KC_NO, KC_EXLM, KC_AT,   KC_HASH, KC_NO,   KC_NO,   KC_1, KC_2,  KC_3,  KC_NO,  KC_NO,
-    QK_BOOT, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_BSPC, KC_NO, KC_0, KC_NO, KC_NO,  KC_NO 
+    QK_BOOT, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_BSPC, KC_0, KC_NO, KC_NO, KC_NO,  KC_NO 
 ),
 [_RAISE] = LAYOUT_planck_grid(
     KC_F1, KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11, KC_F12,
